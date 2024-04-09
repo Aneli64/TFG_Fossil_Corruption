49b30f9f44a954cac69b674fb60789e64e21a0fe
Elian <eliandeniz27@gmail.com>
2024-04-09 22:21:02 +0200

    corregir movimiento del enemigo

--- Scripts/EnemyController.gd
+++ Scripts/EnemyController.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var raycast_left: RayCast2D
+var raycast_right: RayCast2D
+var raycast_forward: RayCast2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,10 +14,34 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	# Iniciamos el sprite de estado estatico una vez inicie el juego
 	movementSprite.play("parado")
+	raycast_left = $LeftRay
+	raycast_right = $RightRay
+	raycast_forward = $RayCastForward
+
+func _left_collision() -> bool:
+	return raycast_left.is_colliding()
+func _right_collision() -> bool:
+	return raycast_right.is_colliding()
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	var move_dir = Vector2.ZERO
+	
+	if _left_collision():
+		print("colision izq")
+		move_dir.x = 1
+	else:
+		move_dir.x = -1
+	if _right_collision():
+		print("colision derech")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		#move_dir.x = 1
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	position += move_dir.normalized() * SPEED * delta
 	move_and_slide()
--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -19,7 +19,7 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		if direction < 0: movementSprite.flip_h = true
