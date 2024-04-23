450b043e2954b3013432be96c9ebfd24c5df9030
Elian <eliandeniz27@gmail.com>
2024-04-23 21:19:43 +0200

    movimiento zombi casi listo

--- Scripts/EnemyController.gd
+++ Scripts/EnemyController.gd
@@ -1,13 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var boolRayCastLeft = false
-var boolRayCastRight = false
 var raycast_left: RayCast2D
 var raycast_right: RayCast2D
-var raycast_forward: RayCast2D
+var raycast_down: RayCast2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,33 +13,30 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	# Iniciamos el sprite de estado estatico una vez inicie el juego
 	movementSprite.play("parado")
+	
+	# Inicializamos los raycast con su correspondiente vector
 	raycast_left = $LeftRay
 	raycast_right = $RightRay
-	raycast_forward = $RayCastForward
+	raycast_down = $MovementSprite/FloorRay
+	
+	# Movimiento por defecto
+	velocity.x = SPEED
+
+func colission():
+	if raycast_right.is_colliding() or !raycast_down.is_colliding():
+		movementSprite.flip_h = true
+		velocity.x = -SPEED
+	elif raycast_left.is_colliding() or !raycast_down.is_colliding():
+		movementSprite.flip_h = false
+		velocity.x = SPEED
 
 func _physics_process(delta):
-	
-	if raycast_left.is_colliding():
-		boolRayCastLeft = true
-		boolRayCastRight = false
-		
-	if raycast_right.is_colliding():
-		boolRayCastRight = true
-		boolRayCastLeft = false
-		
-	# Movimiento
-	velocity.x = -SPEED
-	
-	# Gravedad
+	# Comprobamos colisiones de izquierda y derecha (y no permitimos que caiga )
+	colission()
+	# Controlamos la gravedad
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if boolRayCastLeft:
-		#movementSprite.flip_h = true
-		velocity.x = SPEED
 	
-	if boolRayCastRight:
-		velocity.x = -SPEED
-		
 	
 	move_and_slide()
